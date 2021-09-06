d6aa9811b69103d49cf467a67d52502b6aa38b2e
Ahmet Akyapı <ahmetakyapii@gmail.com>
2021-09-06 13:38:59 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
@@ -35,12 +32,12 @@ class _RandomWordsState extends State<RandomWords> {
   Widget _buildSuggestions() {
   return ListView.builder(
       padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) return const Divider(); /*2*/
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider(); 
 
-        final index = i ~/ 2; /*3*/
+        final index = i ~/ 2;
         if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          _suggestions.addAll(generateWordPairs().take(10));
         }
         return _buildRow(_suggestions[index]);
       });
